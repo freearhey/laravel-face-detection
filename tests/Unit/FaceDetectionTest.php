c6a5a1a8dbeac846d75ce42d35d0a77c55947557
freearhey <free.arhey@gmail.com>
2019-11-07 23:30:55 +0300

    Update FaceDetectionTest.php

--- tests/Unit/FaceDetectionTest.php
+++ tests/Unit/FaceDetectionTest.php
@@ -14,9 +14,9 @@ class FaceDetectionTest extends TestCase
 
   public function setUp(): void
   {
-    $this->normalFilePath = dirname(__FILE__).'/../Data/lena512color.jpg';
-    $this->largeFilePath = dirname(__FILE__).'/../Data/lena1024color.jpg';
-    $this->tmpFilePath = dirname(__FILE__).'/../Data/tmp/face.jpg';
+    $this->normalFilePath = 'tests/Data/lena512color.jpg';
+    $this->largeFilePath = 'tests/Data/lena1024color.jpg';
+    $this->tmpFilePath = 'tests/Data/face.jpg';
   }
 
   public function tearDown(): void
