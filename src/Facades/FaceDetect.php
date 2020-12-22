6b0e09db6266ceb26a42269a0c70328f6aba6b37
freearhey <free.arhey@gmail.com>
2020-12-22 10:06:16 +0300

    Update FaceDetect.php

--- src/Facades/FaceDetect.php
+++ src/Facades/FaceDetect.php
@@ -1,11 +1,15 @@
-<?php 
+<?php
 
 namespace Arhey\FaceDetection\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-class FaceDetection extends Facade {
+class FaceDetection extends Facade
+{
 
-    protected static function getFacadeAccessor() { return 'FaceDetection'; }
+  protected static function getFacadeAccessor()
+  {
+    return 'FaceDetection';
+  }
 
 }
